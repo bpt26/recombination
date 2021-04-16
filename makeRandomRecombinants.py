b6d49c0d4c39116f333033c7c31b58159d782400
Bryan Thornlow <bthornlo@ucsc.edu>
2021-04-16 14:54:53 -0700

    add scripts

---
+++ makeRandomRecombinants.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+# Name: Bryan Thornlow
+# Date: 2/1/2018
+# compareDatabases.py
+
+import sys
+import os
+import datetime
+import numpy
+from numpy import random
+import gzip
+import math
+
+##########################
+###### COMMAND LINE ######
+##########################
+
+class CommandLine(object):
+    """Handles the input arguments from the command line. Manages 
+    the argument parser."""
+
+    def __init__(self, inOpts=None):
+        '''
+        CommandLine constructor.
+        Implements a parser to interpret the command line input using argparse.
+        '''
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-b", "--breakpoints", help="Number of breakpoints that each recombinant sample will have. Must be 1 or 2 (Default = 1).", default=1, type=int)
+        self.parser.add_argument("-s", "--samples", help="Number of recombinant samples to create (Default = 100).", default=100, type=int)
+        self.parser.add_argument("-c", "--copies", help="Number of identical copies to make for each recombinant sample (Default = 10).", default=10, type=int)
+        self.parser.add_argument("-d", "--differences", help="Minimum mutational distance for acceptor/donor samples (Default = 10).", default=10, type=int)
+        self.parser.add_argument("-f", "--fasta", help="Fasta file containing sequences for acceptor/donor samples. [REQUIRED]", default='')
+        self.parser.add_argument("-r", "--ref", help="Fasta file containing reference genome for use in creating VCF. (Default = 'wuhan.ref.fa').", default='wuhan.ref.fa')
+        if inOpts is None:
+            self.args = vars(self.parser.parse_args())
+        else:
+            self.args = vars(self.parser.parse_args(inOpts))
+        self.args = self.parser.parse_args()     
+        if self.args.breakpoints < 1 or self.args.breakpoints > 2:
+            sys.stderr.write("Please retry with either 1 or 2 breakpoints.\n")
+            sys.exit(1)
+
+##########################
+##### MAIN FUNCTIONS #####
+##########################
+
+def makeExamples(myS, myB, myC, myD, myF, myR):
+    posToRef = {}
+    with open(myR) as f:
+        for line in f:
+            l = line.strip().upper()
+            if not l.startswith('>'):
+                myReference = l
+                for i in range(0,len(l)):
+                    l[i] != 'N'
+                    posToRef[i] = l[i]
+            else:
+                myRefName = l[1:]
+
+    sampleToSeq = {}
+    with open(myF) as f:
+        for line in f:
+            l = line.strip()
+            if l.startswith('>'):
+                mySample = l[1:]
+            else:
+                sampleToSeq[mySample] = l
+
+    recSampleToLog = {}
+    recSampleToSeq = {}
+    recSampleToDiffBetweenBps = {}
+    while len(recSampleToSeq) < myS:
+        samples = numpy.random.choice(list(sampleToSeq.keys()), size=2, replace=False)
+        mySampleName = 'RECOMB_'+str(myB)+'_'+str(r)+'_'+str(samples[0])+'_'+str(samples[1])
+        s1 = samples[0]
+        s2 = samples[1]
+        if myB == 1:
+            bps = numpy.random.choice(sorted(list(posToRef.keys()))[5000:-5000],size=1, replace=False)
+            bp1 = bps[0]
+            mySeq = sampleToSeq[s1][:bp1]+sampleToSeq[s2][bp1:]
+            myDiff = minLen(getDiff(sampleToSeq[s1][:bp1], sampleToSeq[s2][:bp1], 0), getDiff(sampleToSeq[s1][bp1:], sampleToSeq[s2][bp1:], bp1))
+        elif myB == 2:
+            bps = sorted(numpy.random.choice(sorted(list(posToRef.keys()))[5000:-5000],size=2, replace=False))
+            if bps[1]-bps[0] <= 1000:
+                bps = sorted(numpy.random.choice(sorted(list(posToRef.keys()))[5000:-5000],size=2, replace=False))
+            bp1 = bps[0]
+            bp2 = bps[1]
+            mySeq = sampleToSeq[s1][:bp1]+sampleToSeq[s2][bp1:bp2]+sampleToSeq[s1][bp2:]
+            myDiff =  minLen(getDiff(sampleToSeq[s1][bp1:bp2], sampleToSeq[s2][bp1:bp2], bp1), getDiff(sampleToSeq[s1][:bp1]+sampleToSeq[s1][bp2:], sampleToSeq[s2][:bp1]+sampleToSeq[s2][bp2:], 0))
+        if myDiff >= myD:
+            recSampleToLog[mySampleName] = [samples, bps]
+            recSampleToSeq[mySampleName] = mySeq
+            recSampleToDiffBetweenBps[mySampleName] = myDiff
+
+    myOutMSA = '>'+myRefName+'\n'+myReference+'\n'
+    myOutLog = ''
+    myOutDiff = ''
+
+    for s in recSampleToSeq:
+        for x in range(0,myC):
+            myOutMSA += '>'+s+'_X'+str(x)+'\n'+recSampleToSeq[s]+'\n'
+        myOutLog += s+'\t'+doubleJoiner(recSampleToLog[s])+'\n'
+        myOutDiff += s+'\t'+joinerC(recSampleToDiffBetweenBps[s])+'\n'
+    open('recombination_'+str(myB)+'.msa.fa','w').write(myOutMSA)
+    open('recombination_'+str(myB)+'.log','w').write(myOutLog)
+    open('recombination_'+str(myB)+'.differences.txt','w').write(myOutDiff)
+
+##########################
+#### HELPER FUNCTIONS ####
+##########################
+
+def addMut(seq, pos):
+    print(pos, len(seq))
+    myReturn = []
+    for i in range(0,len(seq)):
+        if i != int(pos):
+            myReturn.append(seq[i])
+        else:
+            if seq[i] == 'A':
+                mySub = numpy.random.choice(['C','G','T'], size=1)[0]
+            elif seq[i] == 'C':
+                mySub = numpy.random.choice(['A','G','T'], size=1)[0]
+            elif seq[i] == 'G':
+                mySub = numpy.random.choice(['C','A','T'], size=1)[0]
+            elif seq[i] == 'T':
+                mySub = numpy.random.choice(['A','G','C'], size=1)[0]
+            else:
+                mySub = numpy.random.choice(['A','G','C','T'], size=1)[0]
+            myMut = seq[i]+str(pos)+mySub
+            myReturn.append(mySub)
+    return(''.join(myReturn), myMut)
+
+def getDiff(s1, s2, add):
+    myReturn = []
+    for i in range(0,len(s1)):
+        if s1[i] != s2[i]:
+            myReturn.append(add+i)
+    return(myReturn)
+
+def minLen(l1, l2):
+    if len(l1) <= len(l2):
+        return(l1)
+    else:
+        return(l2)
+
+def doubleJoiner(myList):
+    myReturn = []
+    for k in myList:
+        myReturn.append(joiner(k))
+    return(joiner(myReturn))
+
+def replaceSymbols(myEntry):
+    myEntry = myEntry.replace('|', '_')
+    myEntry = myEntry.replace('/', '_')
+    return(myEntry)
+
+def joiner(entry):
+    newList = []
+    for k in entry:
+        newList.append(str(k))
+    return '\t'.join(newList)
+
+def joinerU(entry):
+    newList = []
+    for k in entry:
+        newList.append(str(k))
+    return '_'.join(newList)
+
+def joinerC(entry):
+    newList = []
+    for k in entry:
+        newList.append(str(k))
+    return(','.join(newList))
+
+#########################
+##### FUNCTION CALL #####
+#########################
+
+def main():
+    def main(myCommandLine=None):
+    """
+    Initializes a CommandLine object and passes the provided 
+    arguments into a new fileConverter object and calls main method.
+    """
+    myCommandLine = CommandLine()
+
+    # Necessary files:
+    if myCommandLine.args.samples:
+        myS = myCommandLine.args.samples
+    if myCommandLine.args.breakpoints:
+        myB = myCommandLine.args.breakpoints
+    if myCommandLine.args.copies:
+        myC = myCommandLine.args.copies
+    if myCommandLine.args.differences:
+        myD = myCommandLine.args.differences
+    if myCommandLine.args.fasta:
+        myF = myCommandLine.args.fasta
+    if myCommandLine.args.ref:
+        myR = myCommandLine.args.ref
+
+    makeExamples(myS, myB, myC, myF, myR)
+
+if __name__ == "__main__":
+    """
+    Calls main when program is run by user.
+    """
+    main();
+    raise SystemExit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    """
+    Calls main when program is run by user.
+    """
+    main();
+    raise SystemExit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ parseRecombinationResults.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# Name: Bryan Thornlow
+# Date: 2/1/2018
+# compareDatabases.py
+
+import sys
+import os
+import datetime
+import numpy
+from numpy import random
+import gzip
+import math
+
+
+##########################
+###### COMMAND LINE ######
+##########################
+
+class CommandLine(object):
+    """Handles the input arguments from the command line. Manages 
+    the argument parser."""
+
+    def __init__(self, inOpts=None):
+        '''
+        CommandLine constructor.
+        Implements a parser to interpret the command line input using argparse.
+        '''
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-f", "--fasta", help="Fasta file containing sequences for recombinant genomes. [REQUIRED]",default='')
+        self.parser.add_argument("-l", "--log", help="Log file containing recombinant genomes. Format: recombinantSample sample1 sample2 bp1 (bp2)... [REQUIRED]",default='')
+        self.parser.add_argument('-d', '--descendants', help="Descendants file output by findRecombination. (Default = 'descendants.tsv').",default='descendants.tsv')
+        self.parser.add_argument('-r', '--recombination', help="Recombination file output by findRecombination. (Default = 'recombination.tsv').",default='recombination.tsv')
+        if inOpts is None:
+            self.args = vars(self.parser.parse_args())
+        else:
+            self.args = vars(self.parser.parse_args(inOpts))
+        self.args = self.parser.parse_args()        
+
+##########################
+##### MAIN FUNCTIONS #####
+##########################
+
+def parseDescRec(myF, myL, myD, myR):
+    mySamples = {}
+    with open(myF) as f:
+        for line in f:
+            l = line.strip()
+            if l.startswith('>RECOMB'):
+                mySamples[l[1:]] = True
+
+    sampleToBreakPoints = {}
+    with open(myL) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if splitLine[0]+'_X0' in mySamples:
+                sampleToBreakPoints[splitLine[0]+'_X0'] = int(splitLine[3])
+
+    sampleToRecNodeIds = {}
+    with open(myD) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if not splitLine[0].startswith('#'):
+                mySamples = splitLine[1].split(',')
+                for k in mySamples:
+                    if k in sampleToBreakPoints:
+                        if not k in sampleToRecNodeIds:
+                            sampleToRecNodeIds[k] = []
+                        sampleToRecNodeIds[k].append(int(splitLine[0]))
+
+    nodeIdToBreakPoints = {}
+    with open(myR) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if not splitLine[0].startswith('#'):
+                if not int(splitLine[0]) in nodeIdToBreakPoints:
+                    nodeIdToBreakPoints[int(splitLine[0])] = []
+                if 'GENOME_SIZE)' in splitLine[2]:
+                    splitLine[2] = splitLine[2].replace('GENOME_SIZE)', '29903)')
+                tempList = []
+                for k in (splitLine[1][1:-1]).split(','):
+                    tempList.append(int(k))
+                for k in (splitLine[2][1:-1]).split(','):
+                    tempList.append(int(k))
+                nodeIdToBreakPoints[int(splitLine[0])].append(tempList)
+
+    sampleToCorrectBreakPoints = {}
+    for s in sampleToBreakPoints:
+        sampleToCorrectBreakPoints[s] = False
+        bp = sampleToBreakPoints[s]
+        for n in sampleToRecNodeIds[s]:
+            for l in nodeIdToBreakPoints[n]:
+                if (bp > l[0] and bp < l[1]) or (bp > l[2] and bp < l[3]):
+                    sampleToCorrectBreakPoints[s] = True
+
+    tf = [0,0]
+    for k in sampleToCorrectBreakPoints:
+        if sampleToCorrectBreakPoints[k] == True:
+            tf[0] += 1
+        else:
+            tf[1] += 1
+            print(k)
+    print(tf)
+
+
+def parseDescRec2(myF, myL, myD, myR):
+    mySamples = {}
+    with open(myF) as f:
+        for line in f:
+            l = line.strip()
+            if l.startswith('>RECOMB'):
+                mySamples[l[1:]] = True
+
+    sampleToBreakPoints = {}
+    with open(myL) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if splitLine[0]+'_X0' in mySamples:
+                sampleToBreakPoints[splitLine[0]+'_X0'] = [int(splitLine[3]), int(splitLine[4])]
+
+    sampleToRecNodeIds = {}
+    with open(myD) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if not splitLine[0].startswith('#'):
+                mySamples = splitLine[1].split(',')
+                for k in mySamples:
+                    if k in sampleToBreakPoints:
+                        if not k in sampleToRecNodeIds:
+                            sampleToRecNodeIds[k] = []
+                        sampleToRecNodeIds[k].append(int(splitLine[0]))
+
+    nodeIdToBreakPoints = {}
+    with open(myR) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            if not splitLine[0].startswith('#'):
+                if not int(splitLine[0]) in nodeIdToBreakPoints:
+                    nodeIdToBreakPoints[int(splitLine[0])] = []
+                if 'GENOME_SIZE)' in splitLine[2]:
+                    splitLine[2] = splitLine[2].replace('GENOME_SIZE)', '29903)')
+                tempList = []
+                for k in (splitLine[1][1:-1]).split(','):
+                    tempList.append(int(k))
+                for k in (splitLine[2][1:-1]).split(','):
+                    tempList.append(int(k))
+                nodeIdToBreakPoints[int(splitLine[0])].append(tempList)
+
+    sampleToCorrectBreakPoints = {}
+    for s in sampleToBreakPoints:
+        sampleToCorrectBreakPoints[s] = False
+        bp1 = sampleToBreakPoints[s][0]
+        bp2 = sampleToBreakPoints[s][1]
+        for n in sampleToRecNodeIds[s]:
+            for l in nodeIdToBreakPoints[n]:
+                if (bp1 > l[0] and bp1 < l[1] and bp2 > l[2] and bp2 < l[3]):
+                    sampleToCorrectBreakPoints[s] = True
+
+    tf = [0,0]
+    for k in sampleToCorrectBreakPoints:
+        if sampleToCorrectBreakPoints[k] == True:
+            tf[0] += 1
+        else:
+            tf[1] += 1
+            print(k)
+    print(tf)
+
+##########################
+#### HELPER FUNCTIONS ####
+##########################
+
+def replaceSymbols(myEntry):
+    myEntry = myEntry.replace('|', '_')
+    myEntry = myEntry.replace('/', '_')
+    return(myEntry)
+
+def toInt(entry):
+    myReturn = []
+    for k in entry:
+        myReturn.append(int(k))
+    return(myReturn)
+
+def joiner(entry):
+    newList = []
+    for k in entry:
+        newList.append(str(k))
+    return '\t'.join(newList)
+
+def joinerU(entry):
+    newList = []
+    for k in entry:
+        newList.append(str(k))
+    return '_'.join(newList)
+
+def joinerC(entry):
+    newList = []
+    for k in entry:
+        newList.append('"'+str(k)+'"')
+    return ','.join(newList)
+
+#########################
+##### FUNCTION CALL #####
+#########################
+
+def main():
+    def main(myCommandLine=None):
+    """
+    Initializes a CommandLine object and passes the provided 
+    arguments into a new fileConverter object and calls main method.
+    """
+    myCommandLine = CommandLine()
+
+    # Necessary files:
+    if myCommandLine.args.fasta:
+        myF = myCommandLine.args.fasta
+    if myCommandLine.args.log:
+        myL = myCommandLine.args.log
+    if myCommandLine.args.descendants:
+        myD = myCommandLine.args.descendants
+    if myCommandLine.args.recombination:
+        myR = myCommandLine.args.recombination
+
+    with open(myL) as f:
+        for line in f:
+            splitLine = (line.strip()).split('\t')
+            myLen = len(splitLine)
+    if myLen == 4:
+        parseDescRec(myF, myL, myD, myR)
+    else:
+        parseDescRec2(myF, myL, myD, myR)
+
+if __name__ == "__main__":
+    """
+    Calls main when program is run by user.
+    """
+    main();
+    raise SystemExit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
